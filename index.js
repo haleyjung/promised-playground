2de9aa04e03e414d86bd672f005c9cc42ab2aa66
Jessie Mason <jmason.p@gmail.com>
2022-03-17 11:34:17 -0700

    Basic server setup

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+const app = express()
+const port = 3000
+app.use(express.static('public'))
+app.use(express.json());
+
+const storage = {
+  message: 'Hello World'
+};
+
+app.get('/one', (req, res) => {
+  res.status(200).send(req.query.upper === false ? storage.message.toUpperCase() : storage.message);
+});
+
+app.post('/two', (req, res) => {
+  if(req.body.key && req.body.value){
+    storage[req.body.key] = req.body.value;
+    res.status(200).send(`Successfully stored ${req.body.key} with value ${req.body.value}`);
+  } else {
+    res.status(400).send(`Missing 'key' and/or 'value' on request body.`);
+  }
+});
+
+app.put('/three', (req, res) => {
+  if (req.body.message) {
+    storage.message = req.body.message;
+    res.status(200).send(`Successfully updated stored message to ${req.body.message}`);
+  } else {
+    res.status(400).send(`Missing 'message' on request body.`);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
